d4abab34fd6508676e1c4de0da312731e9ae7a7e
Rewan-Adel <96265779+Rewan-Adel@users.noreply.github.com>
2023-07-25 05:49:28 +0300

    search.js

--- src/routes/search.js
+++ src/routes/search.js
@@ -1,6 +1,7 @@
 const router  = require('express').Router()
 const controller = require('../controllers/search')
 
+// GET  http://localhost:8000/api/search/
 router.get("/name?/:key", controller.srchByName)
 router.get("/brand?/:key", controller.srchByBrand)
 router.get("/category?/:key", controller.srchByCategory)
