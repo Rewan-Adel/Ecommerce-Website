ff664e18627ba3e93ca66270e4a92ee85c3daebc
Rewan-Adel <96265779+Rewan-Adel@users.noreply.github.com>
2023-07-25 03:46:34 +0300

    product.js

--- src/models/product.js
+++ src/models/product.js
@@ -1,11 +1,20 @@
 const mongoose = require("mongoose")
 
 const ProductSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "users",
+  },
     name        : {type: String, unique: true, required : true},
     category    : {type: String, required : true},
+    brand       : {type: String, required : true},
     description : {type: String, required : true},
     price       : {type: String, required : true},
-    avatar      : {type: String }
+    avatar      : {type: String },
+    rating      :  {type:Number, defualt:0},
+    comment     :  {type: String }, 
+    countInStock: { type: Number, required: true, default: 0 },
 },{
     timestamps : true
   }
