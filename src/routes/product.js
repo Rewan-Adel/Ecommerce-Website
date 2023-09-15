6a71dd09943468ef0771a9e8c4f1a057622b81c0
rewan adel <rewanadel1266@gmail.com>
2023-09-15 15:56:17 +0300

    token

--- src/routes/product.js
+++ src/routes/product.js
@@ -3,21 +3,25 @@ const router = express.Router()
 const controller = require('../controllers/productCtr')
 const {verifyToken, isAdmin} = require('../util/token');
 
-router.get("/",             verifyToken, controller.get_all_products);
-router.get("/:id",          verifyToken, controller.get_product_ById);
-router.patch("/:id/review", verifyToken, controller.createProductReview);
+router.use(verifyToken);    
+
+router.get("/",            controller.get_all_products);
+router.get("/:id",         controller.get_product_ById);
+router.patch("/:id/review",controller.createProductReview);
 
 // @desc Search 
 // @route POST api/product/search/:key
-router.get("/search/name/:key",    verifyToken,  controller.get_product_ByName);
-router.get("/search/brand/:key",   verifyToken,  controller.get_product_ByBrand);
-router.get("/search/rating/:key",  verifyToken,  controller.get_product_ByRating);
-router.get("/search/price/:key",   verifyToken,  controller.get_product_ByPrice);
-router.get("/search/category/:key",verifyToken, controller.get_product_ByCategory);
+router.get("/search/name/:key",      controller.get_product_ByName);
+router.get("/search/brand/:key",     controller.get_product_ByBrand);
+router.get("/search/rating/:key",    controller.get_product_ByRating);
+router.get("/search/price/:key",     controller.get_product_ByPrice);
+router.get("/search/category/:key",  controller.get_product_ByCategory);
+
+router.use(isAdmin);    
 
 //@desc Admin Ops
-router.post("/",      verifyToken, isAdmin, controller.addProduct);
-router.patch("/:id",  verifyToken, isAdmin, controller.update_product_ById);
-router.delete("/:id", verifyToken, isAdmin, controller.deleteProduct);
+router.post("/",      controller.addProduct);
+router.patch("/:id",  controller.update_product_ById);
+router.delete("/:id", controller.deleteProduct);
 
 module.exports = router ;
