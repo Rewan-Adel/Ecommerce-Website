9ccbf37f5fbc81d02dee93cd4b7033842c46242f
rewan adel <rewanadel1266@gmail.com>
2023-09-20 10:08:04 +0300

    cart

--- src/models/cartModel.js
+++ src/models/cartModel.js
@@ -1,18 +1,12 @@
 const mongoose = require("mongoose");
 
-const itemSchema = new mongoose.Schema({
-    productId : { type: mongoose.Schema.Types.ObjectId, ref:'products',  required: true},
-    count     : { type: Number, default : 1,  required: true},
-    price     : {type:Number,  required: true}
-},{
-    timestamp : true
-  }
-);
-
 const cartSchema = new mongoose.Schema({
-    userId : {type: mongoose.Schema.Types.ObjectId, ref :"users", required : true},
-    items  : [itemSchema],   
-    cartTotal : {type:Number, required: true}
+    userId : {type: mongoose.Schema.Types.ObjectId, ref :"users"},
+    items  : [ {
+      productId : { type: mongoose.Schema.Types.ObjectId, ref:'products'},
+      count     : { type: Number, default : 1}
+    }],   
+    totalPrice : {type:Number}
 },{
     timestamp : true
   }
